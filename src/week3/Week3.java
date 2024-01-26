c32c7560e35bb6c36a37685380d1dd47df023697
sindevje <sindevje@sheridancollege.ca>
2024-01-26 12:42:55 -0500

    git ignore fole

---
+++ src/week3/Week3.java
@@ -0,0 +1,16 @@
+package week3;
+
+/**
+ *
+ * @author devje
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+    
+}
